116e642e32a56f0abcd6397a230a9546a9df6638
Andy Ball <932400399@bun2-206-14.local>
2019-09-18 10:52:26 +1200

    fixed errors caused by my own sloppy merging

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
     console.log(`We're connected to MongoDB!`);
+});
 
 app.use(function(req, res, next){
     console.log(`${req.method} request for ${req.url}`);
