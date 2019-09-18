13db1c6be277bfe99ebf54563781dd1f8ad82338
Ubuntu <vagrant@ubuntu-xenial.localdomain>
2019-09-18 01:33:30 +0000

    added users model

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ const bcrypt = require('bcryptjs');
 const mongoose = require('mongoose');
 const multer = require('multer');
 
+const User = require('./models/users');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(cors())
@@ -36,3 +38,40 @@ app.use(function(req, res, next){
 app.get('/', function(req, res){
     res.send('Welcome to our Products API. Use endpoints to filter out the data');
 });
+
+app.post('/users',function(req,res){
+  User.findOne({ username:req.body.username}, function(err, result){
+    if (result) {
+        res.send('Sorry, this is already existed');
+    } else {
+      const hash = bcrypt.hashSync(req.body.password);
+      const user = new User({
+        _id: new mongoose.Types.ObjectId(),
+        username: req.body.username,
+        email: req.body.email,
+        password: hash
+      });
+      user.save().then(result => {
+        res.send(result)
+      }).catch(err => res.send(err))
+    }
+  })
+})
+
+app.get('/getUser', function(req,res){
+  const username = req.body.username;
+  const password = req.body.password;
+  User.findOne({ username: username}, function(err, checkUser){
+    if (checkUser) {
+        if (bcrypt.compareSync(req.body.password,checkUser.password)) {
+          console.log('password matched');
+          res.send(checkUser)
+        } else {
+          console.log('password does not matched');
+          res.send('Invalid password');
+        }
+    } else {
+      res.send('Invalid user')
+    }
+  })
+})
