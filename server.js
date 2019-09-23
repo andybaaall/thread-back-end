bda899dd298dd5f97f7ec81c68735ba62ad36964
John Bretherton <270022735@bun2-206-24.local>
2019-09-24 10:39:30 +1200

    tidying up CRUD endpoints for user, item

--- server.js
+++ server.js
@@ -11,9 +11,7 @@ const multer = require('multer');
 const Schema = mongoose.Schema;
 const User = require('./models/users');
 const Item = require('./models/items');
-
-// const Story = mongoose.model('Story', storySchema);
-// const Person = mongoose.model('Person', personSchema);
+const Comment = require('./models/comments');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
@@ -80,6 +78,63 @@ app.post('/users',function(req,res){
   });
 });
 
+//READ ALL USERS
+//////////////////////
+app.get('/allUsers', function(req, res){
+    console.log('working');
+    User.find().then(result => {
+        res.send(result);
+    })
+});
+
+// VALIDATE A USER
+//////////////////////
+app.post('/getUser', function(req,res){
+    User.findOne({username: req.body.username}, function(err, getUser){
+        if(getUser){
+             if(bcrypt.compareSync(req.body.password, getUser.password)){
+                 res.send(getUser);
+             } else {
+                 console.log('invalid password');
+             }
+        } else {
+            res.send('user does not exist');
+        }
+    });
+});
+
+
+// UPDATE A USER
+////////////////
+app.patch('/users/:id', function(req, res){
+    const id = req.params.id;
+    const hash = bcrypt.hashSync(req.body.password);
+    User.findById(id, function(err, user){
+        // CHECK THE LINE BELOW: Should 'user.id' be 'user_id'? Is "userId" ok?
+        if(user['user.id'] == req.body.userId){
+            const newUser = {
+                username: req.body.username,
+                email: req.body.email,
+                password: hash
+            };
+            User.updateOne({ _id: id}, newUser).then(result => {
+                res.send(result);
+            }).catch(err => res.send(err));
+        } else {
+            res.send('401');
+        }
+    }).catch(err => res.send('Sorry, cannot find user with that id'));
+});
+
+// DELETE A USER
+////////////////
+
+
+
+
+
+
+
 // CREATE A NEW ITEM
 //////////////////////
 app.post('/addItem', function(req, res){
@@ -93,7 +148,7 @@ app.post('/addItem', function(req, res){
                 _id:  new mongoose.Types.ObjectId(),
                 item_name: req.body.itemName,
                 item_description: req.body.itemDescription,
-                clothing_type:   req.body.itemType,
+                clothing_type: req.body.itemType,
                 // image_URL: String,
                 // you need to get Multer working!
                 price: req.body.itemPrice,
@@ -108,7 +163,7 @@ app.post('/addItem', function(req, res){
     // });
 });
 
-//READ ITEMS
+//READ ALL ITEMS
 //////////////////////
 app.get('/allItems', function(req, res){
     console.log('working');
@@ -117,18 +172,18 @@ app.get('/allItems', function(req, res){
     })
 });
 
-//UPDATE Item
+//UPDATE AN ITEM
 //////////////////////
-app.post('/addItem/:id', function(req,res){
-    const id = req.params.id;
-    Item.findById(id, function(err, item){
-        if (item['user_id'] == req.body.userID) {
-            res.send(item);
-        } else {
-            res.send('401')
-        }
-    });
-});
+// app.post('/addItem/:id', function(req,res){
+//     const id = req.params.id;
+//     Item.findById(id, function(err, item){
+//         if (item['user_id'] == req.body.userID) {
+//             res.send(item);
+//         } else {
+//             res.send('401')
+//         }
+//     });
+// });
 
 app.patch('/editItem/:id', function(req,res){
    const id = req.params.id;
@@ -157,43 +212,27 @@ app.patch('/editItem/:id', function(req,res){
 });
 
 
-// VALIDATE A USER
-//////////////////////
-app.post('/getUser', function(req,res){
-    User.findOne({username: req.body.username}, function(err, getUser){
-        if(getUser){
-             if(bcrypt.compareSync(req.body.password, getUser.password)){
-                 res.send(getUser);
-             } else {
-                 console.log('invalid password');
-             }
-        } else {
-            res.send('user does not exist');
-        }
-    });
-});
 
-// Update user details (username, email, password) based on id
-////////////////
-app.patch('/users/:id', function(req, res){
-    const id = req.params.id;
-    const hash = bcrypt.hashSync(req.body.password);
-    User.findById(id, function(err, user){
-        // CHECK THE LINE BELOW: Should 'user.id' be 'user_id'? Is "userId" ok?
-        if(user['user.id'] == req.body.userId){
-            const newUser = {
-                username: req.body.username,
-                email: req.body.email,
-                password: hash
-            };
-            User.updateOne({ _id: id}, newUser).then(result => {
-                res.send(result);
-            }).catch(err => res.send(err));
-        } else {
-            res.send('401');
-        }
-    }).catch(err => res.send('Sorry, cannot find user with that id'));
-});
+// DELETE AN ITEM
+//////////////////////
+
+
+// CREATE A COMMENT
+//////////////////////
+
+
+// READ A COMMENT
+//////////////////////
+
+
+// UPDATE A COMMENT
+//////////////////////
+
+
+// DELETE A COMMENT
+//////////////////////
+
+
 
 app.listen(port, () => {
     console.log(`application is running on port ${port}`);
