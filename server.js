191f7aac45510ba7f99983fad032179a8b16a310
Sophie <48259684+alexsophiekim@users.noreply.github.com>
2019-09-25 23:01:34 +1200

    tomorrow prepared

--- server.js
+++ server.js
@@ -68,7 +68,7 @@ app.post('/addItem', upload.single(`itemImg`), function(req,res){
         image_URL: req.file.path,
         price: req.body.itemPrice,
         condition: req.body.itemCondition,
-        user_id: req.body.userID,
+        user_id: req.body.userId,
         bought: false
     });
 
@@ -179,7 +179,7 @@ app.post('/addItem', upload.single('uploadedImage'),function(req, res){
                 // you need to get Multer working!
                 price: req.body.price,
                 condition: req.body.itemCondition,
-                user_id: req.body.userID,
+                user_id: req.body.userId,
                 bought: req.body.itemBought
             });
             item.save().then(result => {
@@ -201,7 +201,7 @@ app.get('/allItems', function(req, res){
 app.post('/addItem/:id', function(req,res){
     const id = req.params.id;
     Item.findById(id, function(err, item){
-        if (item['user_id'] == req.body.userID) {
+        if (item['user_id'] == req.body.userId) {
             res.send(item);
         } else {
             res.send('401')
@@ -239,7 +239,7 @@ app.patch('/editItem/:id', function(req,res){
     console.log(id);
     Item.findById(id, function(err,item){
       console.log('running update');
-       if (item.user_id == req.body.userID) {
+       if (item.user_id == req.body.userId) {
          const newItem = {
            item_name: req.body.itemName,
            item_description: req.body.itemDescription,
@@ -248,7 +248,7 @@ app.patch('/editItem/:id', function(req,res){
            // you need to get Multer working!
            price: req.body.price,
            condition: req.body.itemCondition,
-           user_id: req.body.userID,
+           user_id: req.body.userId,
            bought: req.body.itemBought
          };
          Item.updateOne({_id: id}, newItem).then(result =>{
@@ -280,19 +280,53 @@ app.delete('/addItem/:id', function(req, res){
 
 // CREATE A COMMENT
 //////////////////////
-
+app.post('/comment', function(req, res) {
+    const comment = new Comment({
+      _id: new mongoose.Types.ObjectId(),
+      comment: req.body.comment
+    });
+    comments.save().then(result => {
+      res.send(result);
+    }).catch(err => res.send(err));
+});
 
 // READ A COMMENT
 //////////////////////
-
+app.get('/allComments', function(req, res){
+    Comment.find().then(result => {
+        res.send(result);
+    });
+});
 
 // UPDATE A COMMENT
 //////////////////////
-
+app.post('/allComments/:id', function(req, res){
+  const id = req.params.id;
+  console.log(id);
+  Comment.findById(id, function(err, comment) {
+    if (comment.user_id == req.body.userId) {
+      res.send(comment)
+    } else {
+      res.send('401')
+    }
+  })
+});
 
 // DELETE A COMMENT
 //////////////////////
-
+app.delete('/allComments/:id', function(req, res){
+    const id = req.params.id;
+    console.log(id);
+    Comment.findById(id, function(err, comment){
+        if(comment.user_id == req.body.userID){
+            Comment.deleteOne({ _id: id }, function (err) {
+                res.send('deleted');
+            });
+        } else {
+            res.send('401');
+        }
+    }).catch(err => res.send('cannot find comments with that id'));
+});
 
 
 app.listen(port, () => {
